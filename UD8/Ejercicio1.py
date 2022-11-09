0abcb5a78284f253608592df64c8372699c7bc0f
alvaaroF <fernandezdecordova.oralv21@cordoba.salesianos.edu>
2022-11-09 21:00:29 +0100

    Me corto la polla

--- UD8/Ejercicio1.py
+++ UD8/Ejercicio1.py
@@ -13,45 +13,64 @@ class Persona():
 
     def getNombre(self):
         return print(self.nombre)
+
     def getEdad(self):
         return print(self.edad)
+
     def getDNI(self):
         return print(self.dni)
 
     def setNombre(self, x):
         self.nombre = x
+
     def setEdad(self, x):
         self.edad = x
+
     def setDNI(self, x):
         self.dni = x
 
+    def validarDNI(self):
+        if type(self.dni[0:8] == str) and type(self.dni[8] == int):
+            n = int(self.dni[0:8])
+            letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+            letra = str(letras[n % 23])
+
+            letraDNI = str(self.dni[8].upper())
+
+            if letraDNI == letra:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def validarNombre(self):
+        if self.nombre.isalpha():
+            return True
+        else:
+            return False
+
+    def validarEdad(self):
+        if type(self.edad == str):
+            return False
+        elif int(self.edad) < 0 and int(self.edad) >= 125:
+            return False
+        else:
+            return True
+
     def muestraDatos(self):
-        return print("\nDatos de la persona:\nNombre: ",self.nombre,"\nEdad: ",self.edad,"\nDNI: ",self.dni)
+        return print("\nDatos de la persona:\nNombre: ", self.nombre, "\nEdad: ", self.edad, "\nDNI: ", self.dni)
 
     def esMayorEdad(self):
-        if (self.edad >= 18):
-            return print("¿Es mayor de edad?",True)
+        if type(self.edad) == int:
+            if (int(self.edad) >= 18):
+                return print("¿Es mayor de edad?", True)
         else:
-            return print("¿Es mayor de edad?",False)
+            return print("¿Es mayor de edad?", False)
 
-    def validarDNI(self):
-        import string
-        numeroDNI = int(self.dni[0:7])
-        letras = {0:"T",1:"R",2:"W",3:"A",4:"G",5:"M",6:"Y",7:"F",8:"P",9:"D",10:"X",
-               11:"B",12:"N",13:"J",14:"Z",15:"S",16:"Q",17:"V",18:"H",19:"L",
-               20:"C",21:"K",22:"E"}
-        resto = numeroDNI % 23
-        letra = letras[resto]
-
-        letraDNI = str(self.dni[8])
-
-        if letraDNI.equals(letra):
-            return print("DNI correcto.")
-        else:
-            return print("DNI incorrecto.")
 
 nombre = input("Introduce el nombre: ")
-edad = int(input("Introduce la edad: "))
+edad = input("Introduce la edad: ")
 dni = input("Introduce el DNI: ")
 
 alvaro = Persona()
@@ -60,8 +79,20 @@ alvaro.setNombre(nombre)
 alvaro.setEdad(edad)
 alvaro.setDNI(dni)
 
-print("Comprobando datos...")
-alvaro.validarDNI()
+contador = 0
+if alvaro.validarDNI() == False:
+    print("El DNI intorducido no es correcto.")
+else:
+    contador = contador + 1
+if alvaro.validarNombre() == False:
+    print("El nombre debe estar formado unicamente por letras.")
+else:
+    contador = contador + 1
+if alvaro.validarEdad() == False:
+    print("La edad debe estar formada unimanete por números postivios y deberá ir desde 0 a 125.")
+else:
+    contador = contador + 1
 
-alvaro.muestraDatos()
-alvaro.esMayorEdad()
+if contador == 3:
+    alvaro.muestraDatos()
+    alvaro.esMayorEdad()
